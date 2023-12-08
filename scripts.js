86346d3db827333233051ec9738b2acdc733dbd1
zacharyyi <145707584+zacharyyi@users.noreply.github.com>
2023-12-08 21:04:15 +0000

    new codespace

---
+++ scripts.js
@@ -0,0 +1,30 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let getRatio = (el) =>
+  window.innerHeight / (window.innerHeight + el.offsetHeight);
+
+gsap.utils.toArray("section").forEach((section, i) => {
+  section.bg = section.querySelector(".bg");
+
+  // the first image (i === 0) should be handled differently because it should start at the very top.
+  // use function-based values in order to keep things responsive
+  gsap.fromTo(
+    section.bg,
+    {
+      backgroundPosition: () =>
+        i ? `50% ${-window.innerHeight * getRatio(section)}px` : "50% 0px"
+    },
+    {
+      backgroundPosition: () =>
+        `50% ${window.innerHeight * (1 - getRatio(section))}px`,
+      ease: "none",
+      scrollTrigger: {
+        trigger: section,
+        start: () => (i ? "top bottom" : "top top"),
+        end: "bottom top",
+        scrub: true,
+        invalidateOnRefresh: true // to make it responsive
+      }
+    }
+  );
+});
